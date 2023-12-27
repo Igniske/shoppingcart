2ff45d36e4fd24adf6800c72b9d8969e28cad3bf
Igniske <104842998+Igniske@users.noreply.github.com>
2023-12-27 10:39:08 -0300

    another change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shoppingcart/',
+  base: 'https://igniske.github.io/shoppingcart/',
   plugins: [react()],
 })
